d047633871205262975ff9e864e905810836a8a0
loperjchris <38229090+loperjchris@users.noreply.github.com>
2019-04-22 23:00:17 -0700

    Add files via upload

---
+++ sps.js
@@ -0,0 +1,149 @@
+/**
+Duncan Rover, Chris Loper
+Final Project - Sonoran Plant Search
+sps.js
+Purpose: 
+**/
+
+"use strict";
+
+(function() {
+	let json;
+
+	window.onload = function () {
+		loadPlants();
+		document.getElementById("home").addEventListener("click", clear);
+		document.getElementById("home").addEventListener("click", loadPlants);
+		document.getElementById("send").onclick = sendComment;
+		document.getElementById("load").onclick = loadComments;
+	}
+
+	function sendComment() {
+		let name = document.getElementById("name").value;
+		let comment = document.getElementById("comment").value;
+		comment = comment.replace(/(\r\n|\n|\r)/gm," ");
+		let folder = document.getElementById("folder").innerHTML;
+		let message = {name: name, 
+			comment: comment,
+			folder: folder};
+		const fetchOptions = {
+			method : 'POST',
+			headers : {
+				'Accept': 'application/json',
+				'Content-Type': 'application/json'
+			},
+			body : JSON.stringify(message)
+		};
+		let url = "http://localhost:3001";
+		fetch(url, fetchOptions)
+			.then(checkStatus)
+			.then(function(responseText) {
+				console.log(responseText);
+		})
+		.catch(function(error) {
+			console.log(error);
+		});
+	}
+
+	/** Loads all of the plants from the web service and displays them on the page**/
+	function loadPlants() {
+		let singlePlant = document.getElementById("singleplant");
+		singlePlant.style.visibility = "hidden";
+		let url = "http://localhost:3000?mode=plants";
+			fetch(url)
+			    .then(checkStatus)
+			    .then(function(responseText) {
+			    	json = JSON.parse(responseText);
+			    	for (let i = 0; i < json.plants.length; i++) {
+			    		let plant = document.createElement("div");
+			    		plant.addEventListener("click", plantInfo);
+			    		plant.addEventListener("click", clear);
+			    		plant.folder = json.plants[i].folder;
+			    		let plantName = document.createElement("p");
+			    		plantName.appendChild(document.createTextNode(json.plants[i].name));
+			    		let plantImage = document.createElement("img");
+			    		plantImage.setAttribute("src", "plants/" + json.plants[i].folder + "/plant.jpg");
+			    		plant.appendChild(plantImage);
+			    		plant.appendChild(plantName);
+			    		document.getElementById("allplants").append(plant);
+			    	}
+			    });
+	}
+	
+	/** Clears the page **/
+	function clear() {
+		document.getElementById("allplants").innerHTML = "";
+		document.getElementById("common").innerHTML = "";
+		document.getElementById("description").innerHTML = "";
+		document.getElementById("commentbox").innerHTML = "";
+		document.getElementById("name").value = "";
+		document.getElementById("comment").value = "";
+		document.getElementById("folder").innerHTML = "";
+	}
+
+	/** Gathers all info about a plant when a plant is clicked **/
+	function plantInfo() {
+		document.getElementById("singleplant").style.visibility = "visible";
+		// INFO
+		let folder = this.folder;
+		let url = "http://localhost:3000?mode=info&title=" + folder;
+		fetch(url)
+				.then(checkStatus)
+				.then(function(responseText) {
+					let json = JSON.parse(responseText);
+					let plantImg = document.createElement("img"); // NEEDS EDIT
+					document.getElementById("plantimage").setAttribute("src", "plants/"+folder+"/plant.jpg");
+					document.getElementById("common").innerHTML = json.plants[0].name;
+					document.getElementById("folder").innerHTML = folder;
+					loadComments();
+					//plantFolderName.style.visibility = "hidden";
+				});
+		// DESCRIPTION
+		url = "http://localhost:3000?mode=description&title=" + folder;
+		fetch(url)
+				.then(checkStatus)
+				.then(function(responseText) {
+					let json = JSON.parse(responseText);
+					document.getElementById("description").innerHTML = json;
+
+				});
+	}
+
+		/** Loads all comments to the page from the js service **/
+	function loadComments() {
+		let folder = document.getElementById("folder").innerHTML;
+		let commentBox = document.getElementById("commentbox");
+		commentBox.innerHTML = "";
+		let url = "http://localhost:3001?mode=" + folder;
+		fetch(url)
+		    .then(checkStatus)
+		    .then(function(responseText) {
+		    	let json = JSON.parse(responseText);
+
+		    	for (let i = 0; i < json.messages.length; i++) {
+		    		if (json.messages[i].name != "") {
+		    			let newCommentDiv = document.createElement("div");
+			    		newCommentDiv.className = "comments";
+			    		let newName = document.createElement("p");
+			    		let newComment =document.createElement("span");
+			    		newName.innerHTML = json.messages[i].name + ": ";
+			    		newComment.innerHTML = json.messages[i].comment;
+			    		newName.appendChild(newComment);
+			    		newCommentDiv.appendChild(newName);
+			    		commentBox.appendChild(newCommentDiv);
+		    		}
+		    	}
+		    });
+	}
+
+	function checkStatus(response) { 
+	    if (response.status >= 200 && response.status < 300) {  
+	        return response.text();
+	    } else if(response.status == 404) {
+	    	return Promise.reject(new Error("That page cannot be found."));
+	    } else {  
+	        return Promise.reject(new Error(response.status+": "+response.statusText)); 
+	    }
+	}
+
+}) ();
---
+++ sps_service.js
@@ -0,0 +1,127 @@
+/**
+Christopher Loper
+18 MAR 2019
+Homework 7
+bestreads_service.js
+Purpose: Creates JSON objects about book review info
+Linked to bestreads.html
+**/
+const express = require("express");
+const app = express();
+const app2 = express();
+var fs = require('fs');
+
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
+
+app2.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", 
+               "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.use(express.static('public'));
+console.log("service started");
+app.get('/', function (req, res) {
+	res.header("Access-Control-Allow-Origin", "*");
+	let plant = req.query.title;
+	let mode = req.query.mode;
+	let directory = fs.readdirSync("plants/");
+	let json = {};
+	if (mode == "info") {
+		json = get_info(plant);
+		res.send(JSON.stringify(json));
+	} else if (mode == "description") {
+		let info = fs.readFileSync("plants/" + plant + "/description.txt", "utf8");
+		res.send(JSON.stringify(info));
+	} else if (mode == "plants") {
+		json = get_plants(directory);
+		res.send(JSON.stringify(json));
+	} 
+});
+
+app2.get('/', function(req, res) {
+	let json = {};
+	let folder = req.query.mode;
+	let file = fs.readFileSync("plants/" + folder + "/comments.txt", "utf8");
+	let lines = file.split("\n");
+	let holder = [];
+
+	// loop through each line and split it to gather name and comment
+	for (let i = 0; i < lines.length; i++) {
+		let line = lines[i].split(":::");
+		let lineInfo = {};
+		lineInfo["name"] = line[0];
+		lineInfo["comment"] = line[1];
+		holder[i] = lineInfo;
+	}
+
+	// create JSON as such:
+	// {"messages" : [{"name" : "Merlin", "comment" : "meow"},
+ 	// {"name" : "Purcy", "comment" : "meow meow meow"},
+	// {"name" : "Merlin", "comment" : "grumble meow"}]}
+	json["messages"] = holder;
+	res.send(JSON.stringify(json)); 
+});
+
+app2.post('/', jsonParser, function (req, res) {
+	res.header("Access-Control-Allow-Origin", "*");
+	let name = req.body.name;
+	let comment = req.body.comment;
+	let folder = req.body.folder;
+	// append name and comment to file as a new line like:
+	// NAME:::COMMENT
+	fs.appendFile("plants/" + folder + "/comments.txt", name + ":::" + comment + "\r\n", function(err) {
+		if (err) {
+			return console.log(err);
+		}
+		console.log("Message was posted");
+	});
+});
+
+app.listen(3000);
+app2.listen(3001);
+
+/** Gets all plants **/
+function get_plants(directory) {
+	let json = {}
+	json["plants"] = [];
+	for (let i = 0; i < directory.length; i++) {
+		let currPlant = {};
+		let folder = directory[i];
+		let infoText = fs.readFileSync("plants/" + folder + "/info.txt", "utf8");
+		let lines = infoText.split("\n");
+		currPlant["name"] = lines[0];
+		currPlant["folder"] = folder;
+		json["plants"].push(currPlant);
+	}
+	return json;
+}
+/** Gets plant information **/
+function get_info(plant) {
+	let json = {};
+	json["plants"] = [];
+	let plants = {};
+	let data = fs.readFileSync("plants/" + plant + "/info.txt", "utf8");
+	let lines = data.split("\n");
+	plants["name"] = lines[0];
+	json["plants"].push(plants);
+	return json;
+}
+/** Get books reviews **/
+function get_reviews(book) {
+	let json = {};
+	json["reviews"] = [];  
+	for (let i = 3; i < fs.readdirSync("books/" + book + "/").length; i++) {
+		let curr = i-2;
+		let reviewText = fs.readFileSync("books/" + book + "/review" + curr + ".txt", "utf8");
+		let lines = reviewText.split("\n");
+		let review = {};
+		review["name"] = lines[0];
+		review["stars"] = lines[1];
+		review["review"] = lines[2];
+		json["reviews"].push(review);
+	}
+	return json;
+}
