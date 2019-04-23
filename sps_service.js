c44c7c9c3b24d35fa8c68319038cbe92b036240b
loperjchris <38229090+loperjchris@users.noreply.github.com>
2019-04-23 00:02:17 -0700

    Update sps_service.js

--- sps_service.js
+++ sps_service.js
@@ -19,7 +19,7 @@ app.use(function(req, res, next) {
   next();
 });
 
-app.use(express.static('public'));
+//app.use(express.static('public'));
 console.log("service started");
 app.get('/', function (req, res) {
 	if (req.query.mode == "plants" || req.query.mode == "description" || req.query.mode == "info") {
